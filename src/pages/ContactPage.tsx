46032ac2b5e14a96609293087d632bbcc66151d7
terrence <terrence.tran0@gmail.com>
2020-10-16 18:28:34 +0200

    remove unused import

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -4,11 +4,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import AlumniLogo from '../components/AlumniLogo';
 
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import ButtonBase from '@material-ui/core/ButtonBase';
-
-
 const useStyles = makeStyles((theme) => ({
     paper: {
       marginTop: theme.spacing(8),
