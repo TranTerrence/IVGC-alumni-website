0c8795f4518ab4994cae43252d55fb0086683b63
terrence <terrence.tran0@gmail.com>
2020-11-14 21:36:32 +0100

    Fixing analytics bug: DONT FORGET TO PUT THE MISSING ENV Variable

--- src/components/Firebase/Firebase.ts
+++ src/components/Firebase/Firebase.ts
@@ -1,6 +1,7 @@
 import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/analytics';
 import { collections } from '../../constants/firebase';
 import { User } from './firebase_interfaces';
 
@@ -11,6 +12,8 @@ const config = {
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 /**
