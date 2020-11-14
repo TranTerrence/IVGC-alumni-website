a12f3ece91b49f756068e353401ee54f203bea76
terrence <terrence.tran0@gmail.com>
2020-11-14 19:40:51 +0100

    remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import SignInPage from './pages/SignInPage';
 import Footer from './components/Footer';
