2dab18b33a0ca072f1ec892d3f016b347037a90c
terrence <terrence.tran0@gmail.com>
2020-11-21 15:20:09 +0100

    Adding function to update claims on value changed

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,28 @@
-import * as functions from 'firebase-functions';
+import admin = require('firebase-admin');
+admin.initializeApp();
+const functions = require('firebase-functions').region("europe-west3");
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
+export const updateClaim = functions.firestore
+  .document('users/{userId}')
+  .onUpdate((change: { after: { data: () => any; }; },
+    context: { params: { userId: string; }; }) => {
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+    const newValue = change.after.data();
+    const customClaims = {
+      role: newValue.role,
+      verified: newValue.verified,
+    };
+
+    // Set custom user claims on this update.
+    return admin.auth().setCustomUserClaims(
+      context.params.userId, customClaims)
+      .then(() => {
+        console.log("Custom Claim updated for " + context.params.userId);
+      })
+      .catch((error: any) => {
+        console.log(error);
+      });
+
+  });
