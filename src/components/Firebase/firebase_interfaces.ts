2a5efa67687d2ef3d6c8c3550a3195938a7f2295
terrence <terrence.tran0@gmail.com>
2020-11-24 10:07:27 +0100

    Gather the Profile Interface into the ProfileContext file

--- src/components/Firebase/firebase_interfaces.ts
+++ src/components/Firebase/firebase_interfaces.ts
@@ -5,16 +5,3 @@ export interface User {
   role: string,
   verified: boolean,
 }
-export interface Profile {
-  uid: string, //Same uid as the user
-  email: string,
-  firstName: string,
-  lastName: string,
-  birthday: firebase.firestore.Timestamp,
-  lastEditDate: firebase.firestore.Timestamp,
-  onBoarding: number,
-  promotion: number,
-
-}
-// Role Management in Firebase
-//https://firebase.google.com/docs/firestore/solutions/role-based-access
