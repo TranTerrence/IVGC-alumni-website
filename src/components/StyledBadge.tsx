a7b428311c56de16a3d6c4a9e710f2a584261321
terrence <terrence.tran0@gmail.com>
2020-11-30 18:23:54 +0100

    add the styled badge for showing an avatar with the online dot

---
+++ src/components/StyledBadge.tsx
@@ -0,0 +1,36 @@
+import { makeStyles, Theme, withStyles, createStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar/Avatar";
+import React from "react";
+import Badge from '@material-ui/core/Badge';
+import { palette } from "../constants/colors";
+
+export const StyledBadge = withStyles((theme: Theme) =>
+  createStyles({
+    badge: {
+      backgroundColor: palette.secondary.main,
+      color: palette.secondary.main,
+      boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+      '&::after': {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        borderRadius: '50%',
+        animation: '$ripple 1.2s infinite ease-in-out',
+        border: '1px solid currentColor',
+        content: '""',
+      },
+    },
+    '@keyframes ripple': {
+      '0%': {
+        transform: 'scale(.8)',
+        opacity: 1,
+      },
+      '100%': {
+        transform: 'scale(2.4)',
+        opacity: 0,
+      },
+    },
+  }),
+)(Badge);
